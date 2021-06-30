79c5b7bc4fb3b83d66e7f77ef33721be1f529768
Ghost <85705200+ghostteam008@users.noreply.github.com>
2021-06-30 14:13:34 +0200

    Add files via upload

---
+++ level 2.py
@@ -0,0 +1,6 @@
+print ("كدا انت عديت اول مرحلة")
+print ("حط الباس عشان تعدي المرحلة دي")
+password=input("password===>")
+if password == "1954876322jaheer" :
+    print("الباس صح روح للينك دا")
+    print("")
