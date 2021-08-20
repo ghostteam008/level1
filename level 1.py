f678d159d0c2cf9ed938287b346f8f2a7f4bb501
Ghost <85705200+ghostteam008@users.noreply.github.com>
2021-08-20 15:16:19 +0200

    Update and rename level 2.py to level 1.py

---
+++ level 1.py
@@ -0,0 +1,8 @@
+pa = input ("password==>")
+########################################
+if pa="gh0st.t3ameg":
+    print ("Congratiolations!!")
+    print ("you had solve level 1 test")
+    print ("take a screenshot and upload in the comments")
+else:
+    print ("password is wrong")
